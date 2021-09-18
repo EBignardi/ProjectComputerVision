453fda3d098813a0ebe6aea4d15bd4aefbc481e4
EBignardi <48327197+EBignardi@users.noreply.github.com>
2021-09-18 16:51:03 +0200

    Update networks.py

--- networks.py
+++ networks.py
@@ -126,7 +126,7 @@ class FeatureRegression(nn.Module):
 
     def forward(self, x):
         x = self.conv(x)
-        x = x.view(x.reshape(0), -1)
+        x = x.reshape(x.size(0), -1)
         x = self.linear(x)
         x = self.tanh(x)
         return x
